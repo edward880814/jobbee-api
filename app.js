25a8f18454409d93591f28d173554276130c5606
Edward Kuan <edward880814@gmail.com>
2023-07-18 14:50:18 +0800

    feat: project initialization

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+
+const connectDatabase = require("./config/database");
+
+//Setting up config.env file variable
+dotenv.config({ path: "./config/config.env" });
+
+//Connecting to database
+connectDatabase();
+
+// Setup body parser
+app.use(express.json());
+
+//Importing all routes
+const jobs = require("./routes/jobs");
+
+app.use("/api/v1", jobs);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(
+    `Server started on port ${PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
---
+++ controllers/jobsController.js
@@ -0,0 +1,23 @@
+const Job = require("../models/jobs");
+
+//Get all Jobs =>  /api/v1/jobs
+exports.getJobs = async (req, res, next) => {
+  const jobs = await Job.find();
+
+  res.status(200).json({
+    success: true,
+    results: jobs.length,
+    data: jobs,
+  });
+};
+
+//Create a new job => /api/v1/job/new
+exports.newJob = async (req, res, next) => {
+  const job = await Job.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    message: "Job created",
+    data: job,
+  });
+};
