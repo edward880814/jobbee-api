d63e5eec77129edd31b996b0b713b8cc09ee65b1
Edward Kuan <edward880814@gmail.com>
2023-07-20 19:51:38 +0800

    feat: send password recovery email

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 //Register a new user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -41,4 +42,46 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   }
 
   sendToken(user, 200, res);
+});
+
+// Forgot Password => /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  //Check user email in database
+  if (!user) {
+    return next(new ErrorHandler("No user found with this email.", 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  //Create reset password url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset linke is as follow: \n\n${resetUrl}\n\n If you have not request this, then please ignore that.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Jobbee-API Password Recovery",
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent successfully to: ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler("Email is not sent."), 500);
+  }
 });
