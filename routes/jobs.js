013e91145b02e9acdda6b7c30d7717767b738615
Edward Kuan <edward880814@gmail.com>
2023-07-20 15:39:25 +0800

    feat: authorize user roles and permissions

--- routes/jobs.js
+++ routes/jobs.js
@@ -12,17 +12,19 @@ const {
   jobStats,
 } = require("../controllers/jobsController");
 
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/jobs").get(getJobs);
 router.route("/job/:id/:slug").get(getJob);
 router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
 router.route("/stats/:topic").get(jobStats);
 
-router.route("/job/new").post(isAuthenticatedUser, newJob);
+router
+  .route("/job/new")
+  .post(isAuthenticatedUser, authorizeRoles("employer", "admin"), newJob);
 
 router
   .route("/job/:id")
-  .put(isAuthenticatedUser, updateJob)
-  .delete(isAuthenticatedUser, deleteJob);
+  .put(isAuthenticatedUser, authorizeRoles("employer", "admin"), updateJob)
+  .delete(isAuthenticatedUser, authorizeRoles("employer", "admin"), deleteJob);
 module.exports = router;
