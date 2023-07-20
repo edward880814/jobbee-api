2b3d09a143ff74d32100e46046b5904a319cbd89
Edward Kuan <edward880814@gmail.com>
2023-07-20 15:18:52 +0800

    feat: protect routes from unauthorized user

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,9 +11,9 @@ const sendToken = (user, statusCode, res) => {
     httpOnly: true,
   };
 
-  if (process.env.NODE_ENV === "production") {
-    options.secure = true;
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   options.secure = true;
+  // }
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
