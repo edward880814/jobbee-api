aefdacf81b2fbe9e03b05ba2bd7e7ae13141ca7d
Edward Kuan <edward880814@gmail.com>
2023-07-21 10:35:57 +0800

    feat: update password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
+const sendToken = require("../utils/jwtToken");
 
 // Get current user profile => /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
@@ -10,4 +11,21 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     success: true,
     data: user,
   });
+});
+
+// Update current user Password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  //Check previous user password
+  const isMatched = await user.comparePassword(req.body.currentPassword);
+
+  if (!isMatched) {
+    return next(new ErrorHandler("Old Password is incorrect.", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendToken(user, 200, res);
 });
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
 
-//Check if the user is authenticated or not
+// Check if the user is authenticated or not
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   let token;
 
@@ -15,7 +15,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   }
 
   if (!token) {
-    return next(new ErrorHandler("Login first to access this resource.", 400));
+    return next(new ErrorHandler("Login first to access this resource.", 401));
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
